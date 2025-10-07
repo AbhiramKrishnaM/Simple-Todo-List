d94e4e36ac35006a42066732cf997597f589059c
AbhiramKrishnaM <abhiramKrishna.8921@gmail.com>
2025-10-07 15:12:17 +0530

    components from shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <>
-      <div>hello world!</div>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="text-2xl font-bold text-gray-700">
+          Hey There Abhiram Lets Finish this tasks today!
+        </div>
+      </div>
     </>
   );
 }
